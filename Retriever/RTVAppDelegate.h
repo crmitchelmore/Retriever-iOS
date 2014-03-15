1dc93f195e8c470326c6c5dc49ea66566d97a5d9
Chris Mitchelmore <chrismitchelmore@gmail.com>
2014-03-15 09:09:35 +0000

    Initial Commit

---
+++ Retriever/RTVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RTVAppDelegate.h
+//  Retriever
+//
+//  Created by Chris Mitchelmore on 15/03/2014.
+//  Copyright (c) 2014 Retriever Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Retriever/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Retriever
+//
+//  Created by Chris Mitchelmore on 15/03/2014.
+//  Copyright (c) 2014 Retriever Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTVAppDelegate class]));
+    }
+}
